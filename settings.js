4979f9a4da5185bf9a18be7a72cf966a21f37df3
Ktmbalap <ktmvalcon@gmail.com>
2024-06-12 12:03:16 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6287531724405','6289531724405','6289531724405'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6289531724405','628192110127'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6289531724405' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'anzul' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'anzul-fdl' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'anzul' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
